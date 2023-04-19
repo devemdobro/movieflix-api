453501318d2e6d94f18b7baf067fe5283f2d07f3
devemdobro <devemdobro@gmail.com>
2023-04-19 19:11:49 -0300

    adicionada ordenação, gênero e lingua do filme

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,16 @@ const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
 
-app.get("/movies", async (req, res) => {
-    const movies = await prisma.movie.findMany();
+app.get("/movies", async (_, res) => {
+    const movies = await prisma.movie.findMany({
+        orderBy: {
+            title: "asc",
+        },
+        include: {
+            genres: true,
+            languages: true
+        }
+    });
     res.json(movies);
 });
 
