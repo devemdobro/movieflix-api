5af35b4864cb1d92a6e7f3730355ae5681760afd
devemdobro <devemdobro@gmail.com>
2023-04-19 19:45:12 -0300

    adicionada criação de filme

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
 
+app.use(express.json());
+
 app.get("/movies", async (_, res) => {
     const movies = await prisma.movie.findMany({
         orderBy: {
@@ -17,6 +19,23 @@ app.get("/movies", async (_, res) => {
     });
     res.json(movies);
 });
+
+app.post("/movies", async (req, res) => {
+    const { title, genre_id, language_id, oscar_count, release_date } = req.body;
+    
+    await prisma.movie.create({
+        data: {
+            title,
+            genre_id,
+            language_id,
+            oscar_count,
+            release_date: new Date(release_date)
+        }
+    });
+
+    res.status(201).send();
+});
+
 
 app.listen(port, () => {
     console.log(`Servidor em execução na porta ${port}`);
