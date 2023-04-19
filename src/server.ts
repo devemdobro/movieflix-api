c6d715cdcc94b3220a8957c968227033ac444555
devemdobro <devemdobro@gmail.com>
2023-04-19 20:15:42 -0300

    adicionada correção de filmes duplicados

--- src/server.ts
+++ src/server.ts
@@ -14,32 +14,45 @@ app.get("/movies", async (_, res) => {
         },
         include: {
             genres: true,
-            languages: true
-        }
+            languages: true,
+        },
     });
     res.json(movies);
 });
 
 app.post("/movies", async (req, res) => {
     const { title, genre_id, language_id, oscar_count, release_date } = req.body;
-    
-    try{
+
+    try {
+        // case insensitive - se a busca for feita por john wick ou John wick ou JOHN WICK, o registro vai ser retornado na consulta
+
+        // case sensitive - se buscar por john wick e no banco estiver como John wick, não vai ser retornado na consulta
+
+        const movieWithSameTitle = await prisma.movie.findFirst({
+            where: { title: { equals: title, mode: "insensitive" } },
+        });
+
+        if (movieWithSameTitle) {
+            return res
+                .status(409)
+                .send({ message: "Já existe um filme cadastrado com esse título" });
+        }
+
         await prisma.movie.create({
             data: {
                 title,
                 genre_id,
                 language_id,
                 oscar_count,
-                release_date: new Date(release_date)
-            }
+                release_date: new Date(release_date),
+            },
         });
-    }catch(error){
-        return res.status(500).send({message: "Falha ao cadastrar um filme"});
+    } catch (error) {
+        return res.status(500).send({ message: "Falha ao cadastrar um filme" });
     }
 
     res.status(201).send();
 });
-
 
 app.listen(port, () => {
     console.log(`Servidor em execução na porta ${port}`);
