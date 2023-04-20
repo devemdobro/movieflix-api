f4ec153fa29f2d1dc323bbc71002a2086c3572e5
devemdobro <devemdobro@gmail.com>
2023-04-20 12:12:27 -0300

    adicionada remoção de filmes

--- src/server.ts
+++ src/server.ts
@@ -87,6 +87,28 @@ app.put("/movies/:id", async (req, res) => {
     res.status(200).send();
 });
 
+app.delete("/movies/:id", async (req, res) => {
+    const id = Number(req.params.id);
+
+    try {
+        const movie = await prisma.movie.findUnique({
+            where: { id }
+        });
+
+        if(!movie){
+            return res.status(404).send({ message: "Filme não encontrado" });
+        }
+
+        await prisma.movie.delete({
+            where: { id }
+        });
+    }catch(error){
+        return res.status(500).send({ message: "Falha ao remover o registro" });
+    }
+    
+    res.status(200).send();
+});
+
 app.listen(port, () => {
     console.log(`Servidor em execução na porta ${port}`);
 });
