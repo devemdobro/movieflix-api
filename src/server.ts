5c3b1168789a96f3a7f6b57928f1956049cedac0
devemdobro <devemdobro@gmail.com>
2023-04-20 15:31:09 -0300

    adicionado filtro de filmes por gênero

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
+import { log } from "console";
 
 const port = 3000;
 const app = express();
@@ -105,6 +106,29 @@ app.delete("/movies/:id", async (req, res) => {
     res.status(200).send();
 });
 
+app.get("/movies/:genreName", async (req, res) => {
+
+    try{
+        const moviesFilteredByGenreName = await prisma.movie.findMany({
+            include: {
+                genres: true,
+                languages: true
+            },
+            where: {
+                genres: {
+                    name: {
+                        equals: req.params.genreName,
+                        mode: "insensitive"
+                    }
+                }
+            }
+        });
+
+        res.status(200).send(moviesFilteredByGenreName);
+    }catch(error){
+        res.status(500).send({ message: "Falha ao filtrar filmes por gênero" });
+    }
+});
 
 app.listen(port, () => {
     console.log(`Servidor em execução na porta ${port}`);
